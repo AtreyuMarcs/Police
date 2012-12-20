6cb8498d2b4b0b29cb129d30dfb31fede01cb899
Elvaron <jonas.janus@googlemail.com>
2012-12-20 19:27:18 +0100

    First commit

---
+++ src/police/PoliceRecord.java
@@ -0,0 +1,41 @@
+package police;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PoliceRecord {
+
+	private String playerName;
+	private List<JailRecord> jailRecords;
+	private List<BanRecord> banRecords;
+	
+	public PoliceRecord(String name) {
+		this.setPlayerName(name);
+		this.setJailRecords(new ArrayList<JailRecord>());
+		this.setBanRecords(new ArrayList<BanRecord>());
+	}
+
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public List<JailRecord> getJailRecords() {
+		return jailRecords;
+	}
+
+	protected void setJailRecords(List<JailRecord> jailRecords) {
+		this.jailRecords = jailRecords;
+	}
+
+	public List<BanRecord> getBanRecords() {
+		return banRecords;
+	}
+
+	protected void setBanRecords(List<BanRecord> banRecords) {
+		this.banRecords = banRecords;
+	}
+}
